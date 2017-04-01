a47a3130348dafac8e054f259c002b4fd011af8f
Kerry Buckley <kerryjbuckley@gmail.com>
2017-04-01 22:32:01 +0100

    Add credo

--- apps/ytd_core/lib/ytd_core/strava.ex
+++ apps/ytd_core/lib/ytd_core/strava.ex
@@ -1,8 +1,17 @@
 defmodule YTDCore.Strava do
+  @moduledoc """
+  Wrapper for calls to the Strava API.
+  """
+
+  alias Strava.{Client, Athlete}
+
+  @doc """
+  Given an access token for an athlete, retrieve their year-to-data mileage.
+  """
   def ytd(token) do
-    client = Strava.Client.new token
-    %{id: id} = Strava.Athlete.retrieve_current client
-    %{ytd_run_totals: %{distance: distance}} = Strava.Athlete.stats id, client
+    client = Client.new token
+    %{id: id} = Athlete.retrieve_current client
+    %{ytd_run_totals: %{distance: distance}} = Athlete.stats id, client
     metres_to_miles distance
   end
 
