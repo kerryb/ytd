38596d94b4cce3115bb1dda043e1bb2b72c36169
Kerry Buckley <kerryjbuckley@gmail.com>
2017-04-01 21:28:37 +0100

    Create core app

---
+++ apps/ytd_core/lib/ytd_core.ex
@@ -0,0 +1,18 @@
+defmodule YTDCore do
+  @moduledoc """
+  Documentation for YTDCore.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> YTDCore.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ apps/ytd_core/test/ytd_core_test.exs
@@ -0,0 +1,8 @@
+defmodule YTDCoreTest do
+  use ExUnit.Case
+  doctest YTDCore
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
