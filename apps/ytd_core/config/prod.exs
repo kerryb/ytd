ef0ceb703ad417c0a2bc3e3d804ee76f3cfb3d84
Kerry Buckley <kerryjbuckley@gmail.com>
2017-04-06 22:16:29 +0100

    Hard-code prod urls

--- apps/ytd_core/config/prod.exs
+++ apps/ytd_core/config/prod.exs
@@ -4,4 +4,4 @@ config :strava,
   client_id: "16264",
   client_secret: "${CLIENT_SECRET}",
   access_token: "${ACCESS_TOKEN}",
-  redirect_uri: "${BASE_URL}/auth"
+  redirect_uri: "http://ytd.kerryb.org/auth"
