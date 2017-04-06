7253b1b7bac516938ad75ed9374582e6b5dc9d44
Kerry Buckley <kerryjbuckley@gmail.com>
2017-04-06 21:58:55 +0100

    Use env vars for config (hopefully)

--- apps/ytd_core/config/prod.exs
+++ apps/ytd_core/config/prod.exs
@@ -2,6 +2,6 @@ use Mix.Config
 
 config :strava,
   client_id: "16264",
-  client_secret: "client-secret-would-be-here",
-  access_token: "access-token-would-be-here",
-  redirect_uri: "http://localhost:4000/auth"
+  client_secret: "${CLIENT_SECRET}",
+  access_token: "${ACCESS_TOKEN}",
+  redirect_uri: "${BASE_URL}/auth"
--- apps/ytd_web/config/prod.exs
+++ apps/ytd_web/config/prod.exs
@@ -2,13 +2,12 @@ use Mix.Config
 
 config :ytd_web, YTDWeb.Endpoint,
   http: [port: 80],
-  url: [host: "example.com", port: 80],
+  url: [host: "ytd.kerryb.org", port: 80],
   cache_static_manifest: "priv/static/manifest.json",
   server: true,
-  root: "."
+  root: ".",
+  secret_key_base: "${SECRET_KEY_BASE}"
 
 config :logger, level: :info
 
 config :phoenix, :serve_endpoints, true
-
-import_config "prod.secret.exs"
