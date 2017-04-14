094860a883d7507e5b44373259652b71d46ec974
Kerry Buckley <kerryjbuckley@gmail.com>
2017-04-14 17:06:11 +0100

    Initial letsencrypt setup

--- apps/ytd_web/lib/ytd_web/endpoint.ex
+++ apps/ytd_web/lib/ytd_web/endpoint.ex
@@ -9,7 +9,7 @@ defmodule YTDWeb.Endpoint do
   # when deploying your static files in production.
   plug Plug.Static,
     at: "/", from: :ytd_web, gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt)
+    only: ~w(css fonts images js favicon.ico robots.txt .well-known)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
