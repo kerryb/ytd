5219aacf0c8796f1fd55dc339d78ffb5754b0186
Kerry Buckley <kerryjbuckley@gmail.com>
2017-04-15 13:40:33 +0100

    Add SSL config

--- apps/ytd_web/config/prod.exs
+++ apps/ytd_web/config/prod.exs
@@ -2,7 +2,11 @@ use Mix.Config
 
 config :ytd_web, YTDWeb.Endpoint,
   http: [port: 80],
-  url: [host: "ytd.kerryb.org", port: 80],
+  https: [port: 443,
+          otp_app: :ytd_web,
+          keyfile: "/etc/letsencrypt/live/ytd.kerryb.org/privkey.pem",
+          certfile: "/etc/letsencrypt/live/ytd.kerryb.org/cert.pem"]
+  url: [host: "ytd.kerryb.org", port: 443],
   cache_static_manifest: "priv/static/manifest.json",
   server: true,
   root: ".",
