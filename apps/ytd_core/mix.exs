547c266628251f75e94bbabb1ea8fb89a830f2ef
Kerry Buckley <kerryjbuckley@gmail.com>
2017-04-19 13:18:24 +0100

    Set MIX ENV again (probably don't need this one either)

--- apps/ytd_core/mix.exs
+++ apps/ytd_core/mix.exs
@@ -4,7 +4,7 @@ defmodule YTDCore.Mixfile do
   def project do
     [
       app: :ytd_core,
-      version: "0.6.1",
+      version: "0.6.2",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
