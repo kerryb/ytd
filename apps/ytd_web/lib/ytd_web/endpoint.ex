234afd3a0427359e0b0e0a35089bcda03e41176f
Kerry Buckley <kerryjbuckley@gmail.com>
2017-04-26 20:29:59 +0100

    Fix static file list to allow digested names

--- apps/ytd_web/lib/ytd_web/endpoint.ex
+++ apps/ytd_web/lib/ytd_web/endpoint.ex
@@ -9,8 +9,8 @@ defmodule YTDWeb.Endpoint do
   # when deploying your static files in production.
   plug Plug.Static,
     at: "/", from: :ytd_web, gzip: false,
-    only: ~w(css fonts images js favicon*.ico favicon-96x96.png
-             apple-icon-180x180.png robots.txt .well-known)
+    only: ~w(css fonts images js favicon*.ico favicon*.png
+             apple-icon*.png robots.txt .well-known)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
