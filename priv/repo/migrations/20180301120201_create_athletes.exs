449801b9b34177f31ebfd6ec96a026fb420cf600
Kerry Buckley <kerryjbuckley@gmail.com>
2018-03-01 20:46:08 +0000

    Create athletes table

---
+++ priv/repo/migrations/20180301120201_create_athletes.exs
@@ -0,0 +1,14 @@
+defmodule YTD.Repo.Migrations.CreateAthletes do
+  use Ecto.Migration
+
+  def change do
+    create table(:athletes) do
+      add(:strava_id, :integer)
+      add(:token, :string)
+      add(:run_target, :integer)
+      add(:ride_target, :integer)
+      add(:swim_target, :integer)
+      timestamps
+    end
+  end
+end
--- priv/repo/structure.sql
+++ priv/repo/structure.sql
@@ -35,6 +35,41 @@ SET default_tablespace = '';
 SET default_with_oids = false;
 
 --
+-- Name: athletes; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE athletes (
+    id bigint NOT NULL,
+    strava_id integer,
+    token character varying(255),
+    run_target integer,
+    ride_target integer,
+    swim_target integer,
+    inserted_at timestamp without time zone NOT NULL,
+    updated_at timestamp without time zone NOT NULL
+);
+
+
+--
+-- Name: athletes_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE athletes_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: athletes_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE athletes_id_seq OWNED BY athletes.id;
+
+
+--
 -- Name: schema_migrations; Type: TABLE; Schema: public; Owner: -
 --
 
@@ -42,6 +77,21 @@ CREATE TABLE schema_migrations (
     version bigint NOT NULL,
     inserted_at timestamp without time zone
 );
+
+
+--
+-- Name: athletes id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY athletes ALTER COLUMN id SET DEFAULT nextval('athletes_id_seq'::regclass);
+
+
+--
+-- Name: athletes athletes_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY athletes
+    ADD CONSTRAINT athletes_pkey PRIMARY KEY (id);
 
 
 --
@@ -55,4 +105,6 @@ ALTER TABLE ONLY schema_migrations
 --
 -- PostgreSQL database dump complete
 --
+
+INSERT INTO "schema_migrations" (version) VALUES (20180301120201);
 
