2e83ee6589261650bec13b61f9c4c2745dd91c98
Kerry Buckley <kerryjbuckley@gmail.com>
2018-03-03 16:02:44 +0000

    Update schema dump (should have happened automatically)

--- priv/repo/structure.sql
+++ priv/repo/structure.sql
@@ -3,13 +3,14 @@
 --
 
 -- Dumped from database version 10.2
--- Dumped by pg_dump version 10.2
+-- Dumped by pg_dump version 10.3
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
 SET idle_in_transaction_session_timeout = 0;
 SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
 SET check_function_bodies = false;
 SET client_min_messages = warning;
 SET row_security = off;
@@ -28,31 +29,81 @@ CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
 COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
 
 
-SET search_path = public, pg_catalog;
-
 SET default_tablespace = '';
 
 SET default_with_oids = false;
 
 --
+-- Name: athletes; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.athletes (
+    id bigint NOT NULL,
+    strava_id integer,
+    token character varying(255),
+    run_target integer,
+    ride_target integer,
+    swim_target integer,
+    inserted_at timestamp without time zone NOT NULL,
+    updated_at timestamp without time zone NOT NULL
+);
+
+
+--
+-- Name: athletes_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public.athletes_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: athletes_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.athletes_id_seq OWNED BY public.athletes.id;
+
+
+--
 -- Name: schema_migrations; Type: TABLE; Schema: public; Owner: -
 --
 
-CREATE TABLE schema_migrations (
+CREATE TABLE public.schema_migrations (
     version bigint NOT NULL,
     inserted_at timestamp without time zone
 );
 
 
 --
+-- Name: athletes id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.athletes ALTER COLUMN id SET DEFAULT nextval('public.athletes_id_seq'::regclass);
+
+
+--
+-- Name: athletes athletes_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.athletes
+    ADD CONSTRAINT athletes_pkey PRIMARY KEY (id);
+
+
+--
 -- Name: schema_migrations schema_migrations_pkey; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
-ALTER TABLE ONLY schema_migrations
+ALTER TABLE ONLY public.schema_migrations
     ADD CONSTRAINT schema_migrations_pkey PRIMARY KEY (version);
 
 
 --
 -- PostgreSQL database dump complete
 --
+
+INSERT INTO "schema_migrations" (version) VALUES (20180301120201);
 
