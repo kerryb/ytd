faef96cc9e1d9f8f0ab8a252207c13019df9a941
Kerry Buckley <kerryjbuckley@gmail.com>
2018-04-07 16:39:05 +0100

    Move Targets tests to a separate module

--- test/ytd/athletes/athletes_test.exs
+++ test/ytd/athletes/athletes_test.exs
@@ -93,46 +93,4 @@ defmodule YTD.AthletesTest do
       assert YTD.Athletes.athlete_data(999) == nil
     end
   end
-
-  describe "YTD.Athletes.set_run_target/2" do
-    test "allows setting of target run mileage" do
-      Athletes.register(%Athlete{strava_id: 123, token: "access-token"})
-      :ok = Athletes.set_run_target(123, 1000)
-      assert Athletes.find_by_strava_id(123).run_target == 1000
-    end
-
-    test "clears the target if set to zero" do
-      Athletes.register(%Athlete{strava_id: 123, token: "access-token"})
-      :ok = Athletes.set_run_target(123, 0)
-      assert is_nil(Athletes.find_by_strava_id(123).run_target)
-    end
-  end
-
-  describe "YTD.Athletes.set_ride_target/2" do
-    test "allows setting of target ride mileage" do
-      Athletes.register(%Athlete{strava_id: 123, token: "access-token"})
-      :ok = Athletes.set_ride_target(123, 1000)
-      assert Athletes.find_by_strava_id(123).ride_target == 1000
-    end
-
-    test "clears the target if set to zero" do
-      Athletes.register(%Athlete{strava_id: 123, token: "access-token"})
-      :ok = Athletes.set_ride_target(123, 0)
-      assert is_nil(Athletes.find_by_strava_id(123).ride_target)
-    end
-  end
-
-  describe "YTD.Athletes.set_swim_target/2" do
-    test "allows setting of target swim mileage" do
-      Athletes.register(%Athlete{strava_id: 123, token: "access-token"})
-      :ok = Athletes.set_swim_target(123, 1000)
-      assert Athletes.find_by_strava_id(123).swim_target == 1000
-    end
-
-    test "clears the target if set to zero" do
-      Athletes.register(%Athlete{strava_id: 123, token: "access-token"})
-      :ok = Athletes.set_swim_target(123, 0)
-      assert is_nil(Athletes.find_by_strava_id(123).swim_target)
-    end
-  end
 end
