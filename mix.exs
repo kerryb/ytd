8c058fe0a718a88c406d8ba7a8e8d9c4e1c27400
Kerry Buckley <kerryjbuckley@gmail.com>
2020-06-20 16:34:54 +0100

    Require Elixir 1.10

--- mix.exs
+++ mix.exs
@@ -4,8 +4,8 @@ defmodule Ytd.MixProject do
   def project do
     [
       app: :ytd,
-      version: "5.0.0",
-      elixir: "~> 1.7",
+      version: "0.1.0",
+      elixir: "~> 1.10",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
