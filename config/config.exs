adcd8ee14569d45314d003640451fd08456f8915
Kerry Buckley <kerryjbuckley@gmail.com>
2020-07-26 21:39:50 +0100

    Spike auth from live view

--- config/config.exs
+++ config/config.exs
@@ -26,6 +26,11 @@ config :logger, :console,
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
+config :strava,
+  client_id: System.get_env("YTD_CLIENT_ID"),
+  client_secret: System.get_env("YTD_CLIENT_SECRET"),
+  redirect_uri: System.get_env("YTD_REDIRECT_URL")
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env()}.exs"
--- lib/ytd_web/live/index_live.ex
+++ lib/ytd_web/live/index_live.ex
@@ -6,7 +6,29 @@ defmodule YtdWeb.IndexLive do
   use YtdWeb, :live_view
 
   @impl true
-  def mount(_params, _session, socket) do
-    {:ok, socket}
+  def mount(params, session, socket) do
+    IO.inspect(params, label: "params")
+    IO.inspect(session, label: "session")
+
+    if connected?(socket) do
+      if params["code"] do
+        client = Strava.Auth.get_token!(code: params["code"], grant_type: "authorization_code")
+        athlete = Strava.Auth.get_athlete!(client)
+        {:ok, assign(socket, name: athlete.firstname)}
+      else
+        send(self(), :auth)
+        {:ok, assign(socket, name: "")}
+      end
+    else
+      {:ok, assign(socket, name: "")}
+    end
+  end
+
+  @impl true
+  def handle_info(:auth, socket) do
+    {:noreply,
+     redirect(socket,
+       external: Strava.Auth.authorize_url!(scope: "activity:read,activity:read_all")
+     )}
   end
 end
