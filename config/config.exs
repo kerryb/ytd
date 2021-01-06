5697d3906067a894302b15f7af61ca114ad8b938
Kerry Buckley <kerryjbuckley@gmail.com>
2021-01-06 22:52:28 +0000

    Increase Strava API timeout to 5 minutes
    Ready to stream activities in one go.

--- config/config.exs
+++ config/config.exs
@@ -29,7 +29,8 @@ config :phoenix, :json_library, Jason
 config :strava,
   client_id: System.get_env("YTD_CLIENT_ID"),
   client_secret: System.get_env("YTD_CLIENT_SECRET"),
-  redirect_uri: System.get_env("YTD_REDIRECT_URL")
+  redirect_uri: System.get_env("YTD_REDIRECT_URL"),
+  recv_timeout: :timer.minutes(5)
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
