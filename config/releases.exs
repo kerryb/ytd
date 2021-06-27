666b5454cd68641ae33abaf249aa7a7f085b72c8
Kerry Buckley <kerryjbuckley@gmail.com>
2021-06-27 19:37:55 +0100

    Move production db config to releases.exs

--- config/releases.exs
+++ config/releases.exs
@@ -7,4 +7,11 @@ secret_key_base =
     You can generate one by calling: mix phx.gen.secret
     """
 
+config :ytd, YTD.Repo,
+  database: "ytd",
+  hostname: "localhost",
+  username: System.get_env("YTD_DATABASE_USERNAME"),
+  password: System.get_env("YTD_DATABASE_PASSWORD"),
+  pool_size: 10
+
 config :ytd, YTDWeb.Endpoint, secret_key_base: secret_key_base, server: true
