a71c90a2e939400419cdf00cf89c13821303d6a3
Kerry Buckley <kerryjbuckley@gmail.com>
2021-06-28 22:06:11 +0100

    Tweak production endpoint config
    * Fix hostname
    * Enable server
    * Set root
    * Allow localhost as origin

--- config/prod.exs
+++ config/prod.exs
@@ -10,8 +10,11 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :ytd, YTDWeb.Endpoint,
-  url: [host: "beta.ytd.kerryb.com", port: 443],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  url: [host: "beta.ytd.kerryb.org", port: 443],
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  server: true,
+  root: ".",
+  check_origin: ["//localhost", "//127.0.0.1"]
 
 # Do not print debug messages in production
 config :logger, level: :info
