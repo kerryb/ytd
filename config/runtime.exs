c10c9da5bb34f3b09ca0710b42fe9f6fc244f2cf
Kerry Buckley <kerryjbuckley@gmail.com>
2021-07-04 10:08:33 +0100

    Rename releases.exs to runtime.exs

---
+++ config/runtime.exs
@@ -0,0 +1,25 @@
+import Config
+
+config :strava,
+  client_id: System.get_env("YTD_STRAVA_CLIENT_ID"),
+  client_secret: System.get_env("YTD_STRAVA_CLIENT_SECRET"),
+  redirect_uri: System.get_env("YTD_STRAVA_REDIRECT_URL"),
+  recv_timeout: :timer.minutes(5)
+
+if System.get_env("RELEASE_MODE") do
+  secret_key_base =
+    System.get_env("YTD_SECRET_KEY_BASE") ||
+      raise """
+      environment variable YTD_SECRET_KEY_BASE is missing.
+      You can generate one by calling: mix phx.gen.secret
+      """
+
+  config :ytd, YTD.Repo,
+    database: "ytd",
+    hostname: "localhost",
+    username: System.get_env("YTD_DATABASE_USERNAME"),
+    password: System.get_env("YTD_DATABASE_PASSWORD"),
+    pool_size: 10
+
+  config :ytd, YTDWeb.Endpoint, secret_key_base: secret_key_base
+end
