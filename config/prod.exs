c6cbb93b4062715d731e8bc097c25725dc023ac7
Kerry Buckley <kerryjbuckley@gmail.com>
2021-11-13 15:56:14 +0000

    Set scheme to https in production

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :ytd, YTDWeb.Endpoint,
-  url: [host: "ytd.kerryb.org", port: 443],
+  url: [host: "ytd.kerryb.org", scheme: "https"],
   http: [port: 4000],
   cache_static_manifest: "priv/static/cache_manifest.json",
   server: true,
